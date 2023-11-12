31509c6544c7b3232a98c7dfc3f6d54e7c37dfae
eren3421 <117022092+eren3421@users.noreply.github.com>
2023-11-12 12:31:05 +0300

    RANDOM

---
+++ yükleme/random02.py
@@ -0,0 +1,15 @@
+import random
+yt=random.randint(1,2)
+tahmin=input("YAZI TURA?")
+if yt==1:
+    print("YAZI")
+else:
+    print("TURA")
+if yt==1:
+    atilan="YAZI"
+else:
+    atilan="TURA"
+if tahmin==atilan:
+    print("KAZANDIN")
+else:
+    print("KAYBETTİN")
